7a135bff63779e34c136ba54d0e50feb7653b747
Enk-sho portfolio <emshowah3@gmail.com>
2026-07-02 21:51:17 -0300

    Add files via upload

---
+++ PasswordHealth.py
@@ -0,0 +1,20 @@
+# string for special characters
+special_characters = "!@#$%^&*"
+
+# Get users password
+
+# Check for length (len)
+
+# check for uppercase letters
+
+# check for numbers
+
+# check for special characters
+
+# Print the final report
+
+# Feedback on collected errors (if/ else statments)
+
+# run the program
+if __name__ == "__main__":
+    pass
