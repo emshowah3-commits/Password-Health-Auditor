fa5333d79d90fd15cfa24e5bfd83d9e45f195b9b
Enk-sho portfolio <emshowah3@gmail.com>
2026-07-09 22:07:06 -0300

    Add files via upload

--- PasswordHealth.py
+++ PasswordHealth.py
@@ -1,19 +1,25 @@
+import re
 # string for special characters
 special_characters = "!@#$%^&*"
 
-# Get users password
+# Get users password and 
+password = input(len("What is you password"))
 
 # Check for length (len)
+# len().password
 
 # check for uppercase letters
-
+has_upper = any(len(char.isupper() for char in password))
 # check for numbers
+def is_number(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
 # check for special characters
 
-# Print the final report
-
-# Feedback on collected errors (if/ else statments)
 
 # run the program
 if __name__ == "__main__":
